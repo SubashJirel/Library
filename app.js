794aeccbff107ae4f0cdb35630b499fbd142595b
Subash Jirel <subashjrl@gmail.com>
2023-04-25 15:10:40 +0545

    made the form and added some form toggle and also used event.preventDefault for the input button of form

---
+++ app.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('.addForm');
+const addBook = document.querySelector('.addBook');
+
+// toggle book add form
+addBook.addEventListener('click', toggleForm);
+function toggleForm() {
+  document.querySelector('fieldset').classList.toggle('hide');
+}
+
+//prevent default for submit
+form.addEventListener('submit', submitThis);
+function submitThis(event) {
+  event.preventDefault();
+}
